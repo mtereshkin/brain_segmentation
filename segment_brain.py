bc4cc6c82428f08bc77b6dcc274ca778be6b0706
MikhailTereshkin <tereshkin.ma@phystech.edu>
2024-06-26 20:54:36 +0200

    big commit

--- segment_brain.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    command = "antsBrainExtraction.sh -d 3 -a CT2.nii.gz -e templates/T_template0.nii.gz  -m templates/T_template0_BrainCerebellumProbabilityMask.nii.gz -o output"
-    subprocess.run(command, shell=True)
